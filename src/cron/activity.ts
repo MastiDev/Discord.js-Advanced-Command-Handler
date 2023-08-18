7ed411a30d7adaff6f1482ab2faddea998dd176f
MastiDev <65887954+MastiDev@users.noreply.github.com>
2023-08-18 12:27:41 +0200

    Add Custom Activity

--- src/cron/activity.ts
+++ src/cron/activity.ts
@@ -1,23 +1,19 @@
-import { Client } from 'discord.js';
+import { Client, ActivityType } from 'discord.js';
 import chalk from 'chalk';
 
 export default {
-	cron: '*/15 * * * * *',
+	cron: '*/10 * * * * *',
 	async execute(client: Client) {
-		const activityList = [
-			`with ${client.guilds.cache.size} servers`,
-			`with ${client.users.cache.size} users`,
-			`with ${client.channels.cache.size} channels`,
-			`with ${client.emojis.cache.size} emojis`,
+		const activityList: [ActivityType, string][] = [
+			[ActivityType.Playing, `${client.users.cache.size} users`],
+			[ActivityType.Playing, `${client.guilds.cache.size} servers`],
+			[ActivityType.Custom, 'Serving you!'],
 		];
     
 		const activity = activityList[Math.floor(Math.random() * activityList.length)];
-
 		if(!client.user) return console.log(chalk.red('[ERROR] ') + chalk.yellow('Client user is undefined.'));
 
-		client.user.setActivity(activity, {
-			name: activity,
-			type: 1
-		});
+		if(activity[0] === ActivityType.Custom) client.user.setActivity(activity[1].toString(), { type: ActivityType.Custom });
+		else client.user.setActivity(activity[1].toString(), { type: activity[0] });
 	}
 };
